b320b9f3e27aaa73a7ab6346785779bb85dfe013
James Ward <james@jamesward.org>
2012-05-02 10:54:46 -0600

    add index view test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -13,7 +13,7 @@ public class ApplicationTest {
     assertThat(status(result)).isEqualTo(OK);
     assertThat(contentType(result)).isEqualTo("text/html");
     assertThat(charset(result)).isEqualTo("utf-8");
-    assertThat(contentAsString(result)).contains("hello, world");
+    assertThat(contentAsString(result)).contains("HELLO, WORLD");
   }
 
 }
