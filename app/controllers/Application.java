db0336a4f16ebe61e30084795262f30ff55cef27
James Ward <james@jamesward.org>
2012-05-02 11:33:22 -0600

    add new UI to create Tasks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,20 @@ import play.mvc.*;
 
 import views.html.*;
 
+import models.Task;
+import play.data.Form;
+
 public class Application extends Controller {
   
   public static Result index() {
     return ok(index.render("hello, world"));
+  }
+
+  public static Result addTask() {
+    Form<Task> form = form(Task.class).bindFromRequest();
+    Task task = form.get();
+    task.save();
+    return redirect(routes.Application.index());
   }
   
 }
