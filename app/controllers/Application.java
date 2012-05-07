e86e52fc1b8332a09845f21d6b489044211cb141
James Ward <james@jamesward.org>
2012-05-07 12:47:10 -0600

    fix indentation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,21 +13,21 @@ import java.util.List;
 import static play.libs.Json.toJson;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("hello, world", form(Task.class)));
-  }
 
-  public static Result addTask() {
-    Form<Task> form = form(Task.class).bindFromRequest();
-    Task task = form.get();
-    task.save();
-    return redirect(routes.Application.index());
-  }
+    public static Result index() {
+        return ok(index.render("hello, world", form(Task.class)));
+    }
 
-  public static Result getTasks() {
-    List<Task> tasks = new Model.Finder(String.class, Task.class).all();
-    return ok(toJson(tasks));
-  }
-  
+    public static Result addTask() {
+        Form<Task> form = form(Task.class).bindFromRequest();
+        Task task = form.get();
+        task.save();
+        return redirect(routes.Application.index());
+    }
+
+    public static Result getTasks() {
+        List<Task> tasks = new Model.Finder(String.class, Task.class).all();
+        return ok(toJson(tasks));
+    }
+
 }
--- test/IndexTest.java
+++ test/IndexTest.java
@@ -8,11 +8,11 @@ import static play.test.Helpers.*;
 
 public class IndexTest {
 
-  @Test
-  public void indexTemplate() {
-      Content html = views.html.index.render("test", new Form(Task.class));
-      assertThat(contentType(html)).isEqualTo("text/html");
-      assertThat(contentAsString(html)).contains("TEST");
-  }
+    @Test
+    public void indexTemplate() {
+        Content html = views.html.index.render("test", new Form(Task.class));
+        assertThat(contentType(html)).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("TEST");
+    }
 
 }
