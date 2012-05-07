f00ff01a11a9eade77d43596b5c2057e82c1d125
James Ward <james@jamesward.org>
2012-05-07 12:53:57 -0600

    fix indentation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,16 +9,16 @@ import models.Task;
 import play.data.Form;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("hello, world"));
-  }
 
-  public static Result addTask() {
-    Form<Task> form = form(Task.class).bindFromRequest();
-    Task task = form.get();
-    task.save();
-    return redirect(routes.Application.index());
-  }
-  
+    public static Result index() {
+        return ok(index.render("hello, world"));
+    }
+
+    public static Result addTask() {
+        Form<Task> form = form(Task.class).bindFromRequest();
+        Task task = form.get();
+        task.save();
+        return redirect(routes.Application.index());
+    }
+
 }
